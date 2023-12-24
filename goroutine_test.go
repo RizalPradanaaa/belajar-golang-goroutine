89be3e89974fab730a6fceb7ce786b93384666e6
RizalPradanaaa <rizalpradanaaaa@gmail.com>
2023-12-24 22:49:02 +0700

    Banyak Goroutine

--- goroutine_test.go
+++ goroutine_test.go
@@ -16,3 +16,18 @@ func TestCreateGoroutine(t *testing.T) {
 
 	time.Sleep(1 * time.Second)
 }
+
+
+// Membuat Banyak Goroutine
+func DisplayNumber(number int)  {
+	fmt.Println("Display", number)
+}
+
+func TestManyGoroutines(t *testing.T) {
+	for i := 0; i < 1000; i++ {
+		go DisplayNumber(i)
+	}
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("Selesai")
+}
