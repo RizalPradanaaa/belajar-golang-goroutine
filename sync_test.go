4baf91ebc7544436e6fc46d19810af35167661ab
RizalPradanaaa <rizalpradanaaaa@gmail.com>
2023-12-25 16:03:30 +0700

    sync.Once

--- sync_test.go
+++ sync_test.go
@@ -25,3 +25,31 @@ func TestWaitGroup(t *testing.T) {
 	group.Wait()
 	println("Complete")
 }
+
+
+// sync.Once
+// Once adalah fitur di Go-Lang yang bisa kita gunakan untuk memastikan bahsa sebuah function di
+// eksekusi hanya sekali
+
+var counter = 0
+func onlyOnce()  {
+	counter ++
+}
+
+func TestOnce(t *testing.T) {
+	var group sync.WaitGroup
+	var once sync.Once
+
+	for i := 1; i <= 100; i++ {
+		go func() {
+			group.Add(1)
+			once.Do(onlyOnce)
+			group.Done()
+		}()
+	}
+
+	group.Wait()
+	println(counter)
+	println("Complete")
+
+}
