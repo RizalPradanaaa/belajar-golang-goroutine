154ead9e8b3702a1ce5b58fe5dd6537379b883cb
RizalPradanaaa <rizalpradanaaaa@gmail.com>
2023-12-25 17:37:24 +0700

    time.Ticker

--- timer_test.go
+++ timer_test.go
@@ -38,3 +38,13 @@ func TestAfterFunc(t *testing.T) {
 
 	group.Wait()
 }
+
+
+// time.Ticker
+func TestTicker(t *testing.T) {
+	ticker := time.NewTicker(1 * time.Second)
+
+	for tick := range ticker.C {
+		fmt.Println(tick)
+	}
+}
